81b30452c97f812df96a9211ecfcc2893d9dfd35
LearningX <28deepakchauhan@gmail.com>
2023-01-22 18:46:35 +0530

    update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,15 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="/src/test/java/features")
+@CucumberOptions(features ="/src/test/java/features",
+monochrome= true,
+tags="@Regression",
+plugin= {"pretty", 
+		"html: target/cucumber-reports",
+		"json:target/cucumber-report/cucumber.json",
+		"junit: target/cucumber-reports/Cucumber.xml"},
+glue="stepDefination")
+
 public class TestRunner {
 
 }
